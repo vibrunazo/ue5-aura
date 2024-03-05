a30130c6c0d934e89a031ad67023bc85a769fa6b
vib <vibrunazo@gmail.com>
2024-03-05 18:12:33 -0300

    29. Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// vib
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// vib
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
