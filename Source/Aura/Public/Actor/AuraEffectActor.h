1ae749e2c98bd22cb807221173d4b5319d7a7355
vib <vibrunazo@gmail.com>
2024-03-12 18:37:53 -0300

    48. Curve Tables for Scalable Floats

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -89,4 +89,7 @@ protected:
 
 	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveEffectHandles;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	float ActorLevel = 1.f;
+
 };
