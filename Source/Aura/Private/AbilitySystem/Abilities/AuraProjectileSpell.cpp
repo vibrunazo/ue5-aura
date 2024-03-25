839b143bc5700b81866864146d2e27f4789adfb1
vib <vibrunazo@gmail.com>
2024-03-24 23:06:48 -0300

    109. Aura Projectile Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,12 @@
+// vib
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+}
