9d05253f7bc8c30d6094a6ce651e21b298e84f05
vib <vibrunazo@gmail.com>
2024-04-03 18:25:32 -0300

    144. Implementing Block Chance

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,14 +4,18 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
 struct AuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance)
+	
 	AuraDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false)
 		
 	}
 };
@@ -25,6 +29,7 @@ static AuraDamageStatics GetDamageStatics()
 UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(GetDamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(GetDamageStatics().BlockChanceDef);
 	
 }
 
@@ -48,8 +53,29 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	float Armor = 0.f;
 	// Will set Armor to the value of the target Armor when hit
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetDamageStatics().ArmorDef, EvaluationParameters, Armor);
-	Armor++;
-	const FGameplayModifierEvaluatedData EvaluatedData(GetDamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+
+	// Armor++;
+	// const FGameplayModifierEvaluatedData EvaluatedData(GetDamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	
+	// Get Damage Set by Caller Magnitude
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+	Damage -= Armor;
+	Damage = FMath::Max(0.f, Damage);
+	
+	// Capture Block chance on the target, and determine if there was a block
+	float BlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetDamageStatics().BlockChanceDef, EvaluationParameters, BlockChance);
+	// TargetASC->GetAttributeSet(UAuraAttributeSet::StaticClass());
+	// BlockChance = 25.f;
+	// If Block, halve Damage
+	const int8 BlockRoll = FMath::RandRange(1, 100);
+	const bool bBlocked = BlockRoll < BlockChance;
+	if (bBlocked) Damage *= 0.5f;
+	// GEngine->AddOnScreenDebugMessage(-1, 6.0f, FColor::Yellow,
+	// 	FString::Printf(TEXT("Block? %d, Roll: %f, Chance: %f"), bBlocked, BlockRoll, BlockChance));
+	// UE_LOG(LogTemp, Warning, TEXT("Block? %d, Roll: %f, Chance: %f"), bBlocked, BlockRoll, BlockChance)
+
+	const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 	
 	
